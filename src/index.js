a08dd6c8c3a0b6b29ca9842864f1de5f1c20f155
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-18 11:44:34 +0100

    Add HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
+   const btn = document.createElement('button');
+ 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+ 
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+ 
+   element.appendChild(btn);
+ 
     return element;
   }
-  
+ 
   document.body.appendChild(component());
